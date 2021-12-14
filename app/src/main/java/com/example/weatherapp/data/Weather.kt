63991572a5f26d915c0df40dcac3fdfc6b8f4f79
HLLW0RLD <fobosnumber1@gmail.com>
2021-12-14 16:24:46 +0300

    первое задание

---
+++ app/src/main/java/com/example/weatherapp/data/Weather.kt
@@ -0,0 +1,5 @@
+package com.example.weatherapp.data
+
+data class Weather(
+    val temperature: Int
+)
---
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.weatherapp.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.weatherapp.R
+import com.example.weatherapp.data.Weather
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn.setOnClickListener {
+
+        }
+    }
+}
